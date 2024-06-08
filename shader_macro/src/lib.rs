e719a62afc680cb9fe935f26a665977dea99c7cb
kittuov <kittuov@gmail.com>
2024-06-09 03:11:41 +0530

    fixed typos

--- shader_macro/src/lib.rs
+++ shader_macro/src/lib.rs
@@ -154,19 +154,20 @@ impl Parse for ShaderMacro {
 /// ## Syntax
 ///
 /// ```
+/// use shader_macro::compile_shader;
 /// compile_shader!{
-///     src: "some shader source code"  // [required] either src or src_file is required
-///     src_file: "path/to/shader/source/code" // [required] either src or src_file is required
-///     entry_point: "main_func_name"   // [required] name of entry point function
-///     target: "shader_profile"        // [required] shader used to compile the shader.
-///     src_name: "path/to/shader/source/file"  // [optional] required for #include if any.
+///     src: "some shader source code",  // [required] either src or src_file is required
+///     src_file: "path/to/shader/source/code", // [required] either src or src_file is required
+///     entry_point: "main_func_name",   // [required] name of entry point function
+///     target: "shader_profile",        // [required] shader used to compile the shader.
+///     src_name: "path/to/shader/source/file",  // [optional] required for #include if any.
 ///                                             // This is auto generated when src_file is used.
 ///     defines: {                      // [optional] used to define shader macros before compiling
 ///         ("DEFINE_1","32"),
 ///     },
 ///     flags1: 0,                      // [optional] flags1
 ///     flags2: 0,                      // [optional] flags2
-///     secondary_data_flags: 0         // [optional] secondary_data_flags
+///     secondary_data_flags: 0,         // [optional] secondary_data_flags
 ///     secondary_data: ""              // [optional] secondary_data
 /// }
 ///
@@ -175,18 +176,19 @@ impl Parse for ShaderMacro {
 /// ## Example usage
 ///
 /// ```
+/// use shader_macro::compile_shader;
 /// let data = compile_shader!{
 ///    src: "
-///         int main() {
-///             return 1;
+///         void main() {
+///             return 0;
 ///         }
 ///     ",
-///     entry_point: "main"
+///     entry_point: "main",
 ///     target: "ps_5_0"
 /// };
 /// ```
 #[proc_macro]
-pub fn shader(_input: TokenStream) -> TokenStream {
+pub fn compile_shader(_input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(_input as ShaderMacroInput);
     let input_name = PCSTR({
         if let Some(name) = &input.src_name {
